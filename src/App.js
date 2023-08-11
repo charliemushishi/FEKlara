e47ac102fb663c9d2d7e3946acc1f80ed14db1a1
Charlie <charliethemushi@gmail.com>
2023-08-11 13:27:18 -0700

    created components for app and api file for axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import React, { useState } from 'react';
 import './App.css';
+import Vault from './components/vault';
+import dummyEmotes from './components/dummyemotes'; 
+import Foundry from './components/foundry'; 
+
+
 
 function App() {
+
+  const [selectedContent, setSelectedContent] = useState('Emote'); // Default selected content
+
+  const handleButtonClick = (content) => {
+    setSelectedContent(content);
+  };
+
+  const renderConsoleContent = () => {
+    if (selectedContent === 'Foundry') {
+      return(
+        <div class="console-box">
+          <Foundry></Foundry>
+        </div>
+      );
+    } else if (selectedContent === 'Forge') {
+      return (
+        <p>its the FORGE</p>
+      );
+    } else if (selectedContent === 'Vault') {
+      return (
+        <div class="console-box">
+        <h1>Emote Vault</h1>
+        <Vault emotes={dummyEmotes} />
+      </div>
+      );
+    }
+  };
+
   return (
     <div className="App">
       <div class="grid-container">
@@ -10,14 +44,16 @@ function App() {
 
         <div class="navigation">navigation
           <div class="nav-buttons">
-          <button class="button">Emote</button>
-          <button class="button">Forge</button>
-          <button class="button">Vault</button>
+          <button class="button" onClick={() => handleButtonClick('Foundry')}>Foundry</button>
+          <button class="button"  onClick={() => handleButtonClick('Forge')}>Forge</button>
+          <button class="button" onClick={() => handleButtonClick('Vault')}>Vault</button>
           </div>
         </div>
         
-        <div class="console">console</div>
-        <div class="console-box">thestuff</div>
+        <div class="console">console
+        {renderConsoleContent()}
+        </div>
+
       </div>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = 'your_backend_url_here'; // update with backend
+
+export const uploadImage = async (file) => {
+    try {
+        const formData = new FormData();
+        formData.append('image', file);
+
+        const response = await axios.post(`${API_URL}/upload`, formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+        });
+
+        return response.data; // Return any relevant data from the response
+    }   catch (error) {
+        throw error; // Handle errors or return specific error messages
+    }
+};
